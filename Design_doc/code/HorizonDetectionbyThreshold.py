c604a85892f37e4ac30cc0665183d922bcb93494
lamki <lamki@oregonstate.edu>
2017-12-01 23:26:59 -0800

    moved comments

--- Design_doc/code/HorizonDetectionbyThreshold.py
+++ Design_doc/code/HorizonDetectionbyThreshold.py
@@ -1,16 +1,27 @@
 def Horizon(directory, image, output=0):
-	imgOg = cv2.imread(str(directory)+image) 						# Read image
+	# Read image
+	imgOg = cv2.imread(str(directory)+image) 						
 	
-	img_YUV = cv2.cvtColor(imgOg,cv2.COLOR_BGR2YCR_CB)				# Convert from BGR to YCRCB
-	b, r, g = cv2.split(img_YUV) 									# Split into blue-green-red channels
-	imgBlueEqu = cv2.merge((cv2.equalizeHist(b), r, g))				# Equalize Blue Channel & Merge channels back
-	img_BGR = cv2.cvtColor(imgOg,cv2.COLOR_YCR_CB2BGR)				# Convert from YCRCB to BGR
+	# Convert from BGR to YCRCB
+	img_YUV = cv2.cvtColor(imgOg,cv2.COLOR_BGR2YCR_CB)
 
-	blur = cv2.GaussianBlur(img_BGR,(5,5),0)						# Perform a gaussian blur
+	# Split into blue-green-red channels			
+	b, r, g = cv2.split(img_YUV)
 
-	img_GREY = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)				# Convert to Greyscale
+	# Equalize Blue Channel & Merge channels back 									
+	imgBlueEqu = cv2.merge((cv2.equalizeHist(b), r, g))	
 
-	ret, thresh = cv2.threshold(img_GREY,120,125,cv2.THRESH_BINARY)	# Threshold image to segment sky
+	# Convert from YCRCB to BGR			
+	img_BGR = cv2.cvtColor(imgOg,cv2.COLOR_YCR_CB2BGR)				
+
+	# Perform a gaussian blur
+	blur = cv2.GaussianBlur(img_BGR,(5,5),0)						
+
+	# Convert to Greyscale
+	img_GREY = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)				
+
+	# Threshold image to segment sky
+	ret, thresh = cv2.threshold(img_GREY,120,125,cv2.THRESH_BINARY)	
 
 	# Perform erosian and dialiation to isolate the sky
 	kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5), (3,3))
@@ -24,7 +35,9 @@ def Horizon(directory, image, output=0):
 		biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
 		print "\t"+str(image)+"\t"+str(cv2.contourArea(biggest_contour))+" \t\tPASS"
 		if output != 0:
-			final = cv2.drawContours(imgOg, biggest_contour, -1, (0,255,0), 3) # Outline elements that may be the sky
-			cv2.imwrite(output+"H_"+image,final)					# Write to file
+			# Outline elements that may be the sky
+			final = cv2.drawContours(imgOg, biggest_contour, -1, (0,255,0), 3)
+			# Write to file
+			cv2.imwrite(output+"H_"+image,final)					
 	else:
 		print "\t"+str(image)+"\t\t\tFAIL"
